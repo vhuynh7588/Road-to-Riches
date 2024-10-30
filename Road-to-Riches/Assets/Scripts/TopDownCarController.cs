feace1521cb5f2675f630d789ce7aefd71e0611f
vhuynh7588 <126136230+vhuynh7588@users.noreply.github.com>
2024-10-29 23:48:22 -0700

    2D car video complete
    Still issue steering

--- Road-to-Riches/Assets/Scripts/TopDownCarController.cs
+++ Road-to-Riches/Assets/Scripts/TopDownCarController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class TopDownCarController : MonoBehaviour
 {
     [Header("Car settings")]
+    public float driftFactor  = 0.95f;
     public float accelerationFactor = 30.0f;
     public float turnFactor = 3.5f;
+    public float maxSpeed = 200;
 
     //Local variables
     float accelerationInput = 0;
     float steeringInput = 0;
 
     float rotationAngle = 0;
+
+    float velocityVsUp = 0;
 
     //Components
     Rigidbody2D carRigidbody2D;
@@ -40,11 +44,28 @@ public class TopDownCarController : MonoBehaviour
     {
         ApplyEngineForce();
 
+        KillOrthogonalVelocity();
+
         ApplySteering();
     }
 
     void ApplyEngineForce()
     {
+        velocityVsUp = Vector2.Dot(transform.up, carRigidbody2D.velocity);
+
+        if(velocityVsUp > maxSpeed && accelerationInput > 0)
+            return;
+
+        if(velocityVsUp < -maxSpeed *0.5f && accelerationInput < 0)
+            return;
+
+        if(carRigidbody2D.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+            return;
+
+        if (accelerationInput == 0)
+            carRigidbody2D.drag = Mathf.Lerp(carRigidbody2D.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else carRigidbody2D.drag = 0;
+
         Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
 
         carRigidbody2D.AddForce(engineForceVector, ForceMode2D.Force);
@@ -52,9 +73,20 @@ public class TopDownCarController : MonoBehaviour
 
     void ApplySteering()
     {
+        float minSpeedBeforeAllowTurningFactor = (carRigidbody2D.velocity.magnitude / 8);
+        minSpeedBeforeAllowTurningFactor = Mathf.Clamp01(minSpeedBeforeAllowTurningFactor);
+
         rotationAngle -= steeringInput * turnFactor;
 
         carRigidbody2D.MoveRotation(rotationAngle);
+    }
+
+    void KillOrthogonalVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(carRigidbody2D.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(carRigidbody2D.velocity, transform.right);
+
+        carRigidbody2D.velocity = forwardVelocity + rightVelocity * driftFactor;
     }
 
     public void SetInputVector(Vector2 inputVector)
