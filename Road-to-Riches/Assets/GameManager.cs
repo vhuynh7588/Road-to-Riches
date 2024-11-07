9dc884be26151e2b2651ddc5e2db5494683a6a14
vhuynh7588 <126136230+vhuynh7588@users.noreply.github.com>
2024-11-06 18:39:31 -0800

    Coin prefab

---
+++ Road-to-Riches/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Text scoreText;  // Reference to a UI Text component for score display
+    private int score = 0;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
